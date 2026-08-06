1ee385d01e7015c419d6934baa04329046701bd1
Luiz Conde <luizconde.deev@gmail.com>
2026-08-06 18:53:27 -0300

    chore: initialize FinanceControlApi repository

---
+++ src/test/java/dev/luizconde/controlefinanceiroapi/ControleFinanceiroApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.luizconde.controlefinanceiroapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleFinanceiroApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
