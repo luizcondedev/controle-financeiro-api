7bdb543dc098f7e78c8f225bc7c8a0fcf4feec50
Luiz Conde <luizconde.deev@gmail.com>
2026-08-07 17:05:07 -0300

    feat(user): add user persistence model

---
+++ src/main/java/dev/luizconde/controlefinanceiroapi/entity/User.java
@@ -0,0 +1,33 @@
+package dev.luizconde.controlefinanceiroapi.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false, length = 100)
+    private String name;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "create_date")
+    private LocalDateTime createDate;
+}
